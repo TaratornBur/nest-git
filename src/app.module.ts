7c6821388ab0f8728c4b5dfdbdf5e1076ffff7b7
Tartorn <sunpopcab@gmail.com>
2021-06-02 16:45:19 +0700

    connect mongoDB part 2 import mongooseModule at appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StrocktidoModule } from './strocktido/strocktido.module';
 
 @Module({
-  imports: [StrocktidoModule],
+  imports: [MongooseModule.forRoot(
+    'mongodb+srv://root:1234@cluster0.4bzcc.mongodb.net/Test1?retryWrites=true&w=majority'
+    ),StrocktidoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/strocktido/strocktido.module.ts
+++ src/strocktido/strocktido.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StrocktidoService } from './strocktido.service';
 import { StrocktidoController } from './strocktido.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Strock, StrockSchema } from './schema/strock.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Strock.name, schema: StrockSchema}])],
   controllers: [StrocktidoController],
   providers: [StrocktidoService]
 })
